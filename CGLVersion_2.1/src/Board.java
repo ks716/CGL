28ba213293ee407d9e1c2780afca175bde25e8fc
ks716 <46082581+ks716@users.noreply.github.com>
2020-12-25 00:14:07 +0530

    update

--- CGLVersion_2.1/src/Board.java
+++ CGLVersion_2.1/src/Board.java
@@ -4,19 +4,33 @@ public class Board {
 	
 	int size;
 	int iter=0;
-	int[] live;
-	Cell[][] board = null;
-	Cell[][] board2 = null;
+//	boolean[][] live;
+//	boolean[][] live2;
+	Cell[][] board;
+	Cell[][] board2;
 	
 	
-	Board(int size) {
-		this.size = size;
-		this.board = new Cell[size][size];
+	Board() {
+		
+		
+		
+		}
 		//this.board2 = new Cell[size][size];
-	}
+
 	
 	void createBoard() {
+//		this.live = new boolean[size][size];
 		this.board = new Cell[size][size];
+		for (int i=0;i<this.size;i++) {
+			for (int j=0;j<this.size;j++) {
+				this.board[i][j] = new Cell();
+				this.board[i][j].row = i;
+				this.board[i][j].col = j;
+			}
+		}
+		
+		
+		
 		
 	}
 	
@@ -44,21 +58,116 @@ public class Board {
 	void nextGen() {
 		this.iter+=1;
 		this.board2 = new Cell[this.size][this.size];
+		System.out.println(this.board.length);
 		for (int i=0;i<this.size;i++) {
 			for (int j=0;j<this.size;j++) {
-				this.board2[i][j] = this.board[i][j].nextGen_status();
+//				this.board[i][j].row = i;
+//				this.board[i][j].col = j;
+		
+//				System.out.println(this.board.length);
+				int alive = this.aliveNCells(i, j);
+				this.board2[i][j] = new Cell();
+
+				if (this.board[i][j].status==false) {
+					if (alive==3) {
+						this.board2[i][j].setStatus(true);
+					}else {
+						this.board2[i][j].setStatus(false);
+					}
+				}else {
+					if (alive==0 || alive==1) {
+						this.board2[i][j].setStatus(false);
+					}else if (alive==2 || alive==3) {
+						this.board2[i][j].setStatus(true);
+					}else {
+						this.board2[i][j].setStatus(false);
+					}
+				}
+				this.board2[i][j].row = i;
+				this.board2[i][j].col = j;
+//				this.board2[i][j] = this.board[i][j].nextGen_status();
+//				this.live2[i][j] = this.board2[i][j].status;
 			}
 		}
 		this.board = null;
 		this.board = Arrays.stream(this.board2).map(a -> Arrays.copyOf(a, a.length)).toArray(Cell[][]::new);
+		
+//		this.live = null;
+//		this.live = Arrays.stream(this.live2).map(a -> Arrays.copyOf(a, a.length)).toArray(boolean[][]::new);
+		this.printBoard();
 	}
 	
 	boolean checkEnd() {
-		if (this.live.length==0) {
-			return true;
+		
+		for(int i=0;i<this.size;i++) {
+			for (int j=0;j<this.size;j++) {
+				if (this.board[i][j].status == true) {
+					return true;
+				}
+			}
+		}
+		
+		return false;	
+	}
+	
+	public int aliveNCells(int row,int col) {
+		int count =0;
+		//this.live_copy = super.board;
+		//System.out.println(super.board.length);
+		if (row == 0 && col == 0) {
+			if (this.board[0][1].status == true) {count++;}
+			if (this.board[1][0].status == true) {count++;}
+			if (this.board[1][1].status == true) {count++;}
+		}else if (row==0 && col==this.size-1) {
+			if (this.board[0][this.size-2].status == true) {count++;}
+			if (this.board[1][this.size-2].status == true) {count++;}
+			if (this.board[1][this.size-1].status == true) {count++;}
+		}else if (row==this.size-1 && col==0) {
+			if (this.board[this.size-2][0].status == true) {count++;}
+			if (this.board[this.size-2][1].status == true) {count++;}
+			if (this.board[this.size-1][1].status == true) {count++;}
+		}else if (row==this.size-1 && col==this.size-1) {
+			if (this.board[this.size-2][this.size-2].status == true) {count++;}
+			if (this.board[this.size-1][this.size-2].status == true) {count++;}
+			if (this.board[this.size-2][this.size-1].status == true) {count++;}
+		}else if (row==0) {
+			for (int i=0;i<=1;i++) {
+				for (int j=-1;j<=1;j++) {
+					if (this.board[row+i][col+j].status==true) {count++;}
+				}
+			}
+			if(this.board[row][col].status==true) {count-=1;}
+		}else if (row==this.size-1) {
+			for (int i=-1;i<1;i++) {
+				for (int j=-1;j<=1;j++) {
+					if (this.board[row+i][col+j].status==true) {count++;}
+				}
+			}
+			if(this.board[row][col].status==true) {count-=1;}
+		}else if (col==0) {
+			for (int i=-1;i<=1;i++) {
+				for (int j=0;j<=1;j++) {
+					if (this.board[row+i][col+j].status==true) {count++;}
+				}
+			}
+			if(this.board[row][col].status==true) {count-=1;}
+		}else if (col==this.size-1) {
+			for (int i=-1;i<=1;i++) {
+				for (int j=-1;j<1;j++) {
+					if (this.board[row+i][col+j].status==true) {count++;}
+				}
+			}
+			if(this.board[row][col].status==true) {count-=1;}
 		}else {
-			return false;
-		}		
+			for (int i=-1;i<=1;i++) {
+				for (int j=-1;j<=1;j++) {
+					if (this.board[row+i][col+j].status==true) {count++;}
+				}
+			}
+			if(this.board[row][col].status==true) {count-=1;}
+		}
+		
+		return count;
 	}
 
 }
--- CGLVersion_2.1/src/Main.java
+++ CGLVersion_2.1/src/Main.java
@@ -1,8 +1,73 @@
-
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Input size of board: ");
+		int size = scan.nextInt();
+		Board game = new Board();
+		game.size =size;
+		game.createBoard();
+		//System.out.print(game.board[0][0].status);
+		System.out.println("How many live cells to start: ");
+		int t = scan.nextInt();
+		for (int i=0;i<t;i++) {
+			if (i==0) {
+				System.out.print("Enter x,y of first cell: ");
+				String s = scan2.nextLine();
+				String s1[] = s.split(",");
+				int x = Integer.parseInt(s1[0]);
+				int y = Integer.parseInt(s1[1]);
+				game.board[x][y].setStatus(true);
+//				game.live[x][y] = (true);
+			}else {
+				System.out.print("Enter x,y of next cell: ");
+				String s = scan2.nextLine();
+				String s1[] = s.split(",");
+				int x = Integer.parseInt(s1[0]);
+				int y = Integer.parseInt(s1[1]);
+				game.board[x][y].setStatus(true);
+//				game.live[x][y] = (true);
+				
+			}
+		}
+		game.printBoard();
+		System.out.println("Game ready, start? Y/N");
+		String reply = "y";
+		//System.out.print(reply);
+		if (reply=="Y" || reply=="y") {
+			System.out.println("***** Game started *****");
+			while(game.checkEnd()) {
+				if (game.iter==500) {
+					break;
+				}
+				System.out.println("Generation: "+game.iter);
+				game.nextGen();
+			}
+			
+			System.out.println("***** Game stopped after "+game.iter+" iterations *****");
+			
+			//start game
+		}else if (reply=="N" || reply=="n") {
+			System.out.println("***** Game stopped *****");
+		}
 		
+		scan2.close();
+		scan.close();
 	}
+	
+//	static void startgame(Board game) {
+//		
+//		while(game.checkEnd()) {
+//			if (game.iter==500) {
+//				break;
+//			}
+//			System.out.println("Generation: "+game.iter);
+//			game.nextGen();
+//		}
+//		
+//		System.out.println("***** Game stopped after "+game.iter+" iterations *****");
+//	}
 }
